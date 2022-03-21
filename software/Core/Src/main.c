012150c05ba2f15112e6c331a8998f53dffa9a83
Calvin <calvinofthejs@gmail.com>
2022-03-21 18:21:40 +1000

    basic steering wheel code implemented

--- software/Core/Src/main.c
+++ software/Core/Src/main.c
@@ -34,17 +34,25 @@
 #include <stdint.h>
 #include <QUTMS_can.h>
 #include <CAN_SW.h>
+#include <CAN_VCU.h>
+#include <obj_dic.h>
+#include <Timer.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
 #define MAX_REGEN_CURRENT 120
 #define MAX_REGEN_DEADZONE 100
+
+void update_buttons();
+
 bool is_btn_pressed(uint8_t idx);
 void button_one_function();
 void button_two_function();
 void button_three_function();
 void button_four_function();
+
+#define DEBOUNCE_MASK 0b00111111
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
@@ -74,45 +82,25 @@ void SystemClock_Config(void);
 uint16_t dma_raw[3];
 uint16_t pot_value[3];
 int pot_result[3];
+uint8_t button_debounce_count[4];
 uint8_t button_current_state[4];
 uint8_t button_prev_state[4];
 char buff[256];
+
+ms_timer_t timer_heartbeat;
+SW_HeartbeatState_t SW_hbState;
+
+void heartbeat_timer_cb(void *args);
+bool is_btn_pressed(uint8_t idx);
+void button_one_function();
+void button_two_function();
+void button_three_function();
+void button_four_function();
 
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc) {
 	if (hadc == &hadc1) {
 		memcpy(pot_value, dma_raw, sizeof(uint16_t) * 3);
 	}
-}
-
-bool is_btn_pressed(uint8_t idx) {
-	if (button_current_state[idx] == 0 && button_prev_state[idx] == 1) {
-		return true;
-	}
-	return false;
-}
-void button_one_function() {
-	int value_results = sprintf(buff, "value 1: %i value 2: %i value 3: %i\r\n",
-			pot_result[0], pot_result[1], pot_result[2]);
-	HAL_UART_Transmit(&huart3, buff, value_results, 2000);
-}
-void button_two_function() {
-	int numchars = sprintf(buff, "This does nothing at the moment\r\n");
-	HAL_UART_Transmit(&huart3, buff, numchars, 2000);
-}
-void button_three_function() {
-	int fan_toggle = sprintf(buff, "Fan button Pressed\r\n");
-	HAL_UART_Transmit(&huart3, buff, fan_toggle, 2000);
-}
-void button_four_function() {
-	int car_horn = sprintf(buff, "Beep Beep! get out of the way\r\n");
-	HAL_UART_Transmit(&huart3, buff, car_horn, 2000);
-
-	SW_ActivateHorn_t msg = Compose_SW_ActivateHorn();
-	CAN_TxHeaderTypeDef header = { .ExtId = msg.id, .IDE =
-	CAN_ID_EXT, .RTR = CAN_RTR_DATA, .DLC = 0,
-			.TransmitGlobalTime = DISABLE };
-
-	HAL_CAN_AddTxMessage(&hcan1, &header, NULL, &txMailbox_CAN1);
 }
 
 /* USER CODE END 0 */
@@ -151,13 +139,20 @@ int main(void) {
 	MX_USART3_UART_Init();
 	/* USER CODE BEGIN 2 */
 
+	SW_hbState.stateID = SW_STATE_START;
+
 	setup_CAN();
 
 	if (HAL_ADC_Start_DMA(&hadc1, (uint32_t*) dma_raw, 3) != HAL_OK) {
 		Error_Handler();
 	}
 
-	char buff[256];
+	timer_heartbeat = timer_init(100, true, heartbeat_timer_cb);
+	timer_start(&timer_heartbeat);
+
+	SW_hbState.stateID = SW_STATE_READY;
+
+
 	/* USER CODE END 2 */
 
 	/* Infinite loop */
@@ -179,13 +174,8 @@ int main(void) {
 
 		/* USER CODE BEGIN 3 */
 
-		//UART transmit takes in as parameters the UART peripheral
-		//the buffer as the data to transmit
-		//numchars as how much data to transmit.
-		button_current_state[0] = HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin);
-		button_current_state[1] = HAL_GPIO_ReadPin(B2_GPIO_Port, B2_Pin);
-		button_current_state[2] = HAL_GPIO_ReadPin(B3_GPIO_Port, B3_Pin);
-		button_current_state[3] = HAL_GPIO_ReadPin(B4_GPIO_Port, B4_Pin);
+		// update button values w/ debouncing
+		update_buttons();
 
 		//int numchars = sprintf(buff, "B1: %i\r\n", state_b1);
 		// int numchars = sprintf(buff, "B1: %i B2: %i B3: %i B4: %i\r\n", button_current_state[0], button_current_state[1], button_current_state[2], button_current_state[3]);
@@ -194,8 +184,8 @@ int main(void) {
 
 		// for(int i = 0; i < 3; i++)
 		//
-		pot_result[0] = pot_value[0] / 4095.0f * 120;
-		pot_result[1] = pot_value[1] / 4095.0f * 100;
+		pot_result[0] = pot_value[0] / 4095.0f * MAX_REGEN_CURRENT;
+		pot_result[1] = pot_value[1] / 4095.0f * MAX_REGEN_DEADZONE;
 
 		//potValues
 
@@ -220,7 +210,7 @@ int main(void) {
 
 		//HAL_UART_Transmit(&huart3, buff, pot_value, 2000);
 
-		HAL_Delay(50);
+		HAL_Delay(1);
 	}
 }
 
@@ -264,6 +254,88 @@ void SystemClock_Config(void) {
 }
 
 /* USER CODE BEGIN 4 */
+
+void update_buttons() {
+	for (int i = 0; i < 4; i++) {
+		button_debounce_count[i] = button_debounce_count[i] << 1;
+		button_debounce_count[i] &= DEBOUNCE_MASK;
+	}
+
+	button_debounce_count[0] |= HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin);
+	button_debounce_count[1] |= HAL_GPIO_ReadPin(B2_GPIO_Port, B2_Pin);
+	button_debounce_count[2] |= HAL_GPIO_ReadPin(B3_GPIO_Port, B3_Pin);
+	button_debounce_count[3] |= HAL_GPIO_ReadPin(B4_GPIO_Port, B4_Pin);
+
+	for (int i = 0; i < 4; i++) {
+		if (button_debounce_count[i] == DEBOUNCE_MASK) {
+			button_current_state[i] = 1;
+		} else if (button_debounce_count[i] == 0) {
+			button_current_state[i] = 0;
+		}
+	}
+}
+
+void heartbeat_timer_cb(void *args) {
+
+	SW_Heartbeat_t msg = Compose_SW_Heartbeat(&SW_hbState);
+
+	CAN_TxHeaderTypeDef header = { .ExtId = msg.id, .IDE =
+		CAN_ID_EXT, .RTR = CAN_RTR_DATA, .DLC = sizeof(msg.data),
+				.TransmitGlobalTime = DISABLE };
+
+		HAL_CAN_AddTxMessage(&hcan1, &header, msg.data, &txMailbox_CAN1);
+}
+
+
+bool is_btn_pressed(uint8_t idx) {
+	if (button_current_state[idx] == 0 && button_prev_state[idx] == 1) {
+		return true;
+	}
+	return false;
+}
+
+void button_one_function() {
+	uint16_t max_regen = pot_result[0];
+
+	uint8_t data[8];
+	OD_generateCAN(OD_MSG_SET, VCU_OD_ID_CTRL_REGEN_MAX_CURRENT, &max_regen,
+			OD_TYPE_UINT16, data);
+
+	VCU_OBJ_DICT_t msg = Compose_VCU_OBJ_DICT(VCU_ID_CTRL, data);
+
+	CAN_TxHeaderTypeDef header = { .ExtId = msg.id, .IDE =
+	CAN_ID_EXT, .RTR = CAN_RTR_DATA, .DLC = sizeof(msg.data),
+			.TransmitGlobalTime = DISABLE };
+
+	HAL_CAN_AddTxMessage(&hcan1, &header, msg.data, &txMailbox_CAN1);
+
+	//int value_results = sprintf(buff, "value 1: %i value 2: %i value 3: %i\r\n",
+	//		pot_result[0], pot_result[1], pot_result[2]);
+	//HAL_UART_Transmit(&huart3, buff, value_results, 2000);
+}
+
+void button_two_function() {
+	//int numchars = sprintf(buff, "This does nothing at the moment\r\n");
+	//HAL_UART_Transmit(&huart3, buff, numchars, 2000);
+}
+
+void button_three_function() {
+	SW_hbState.flags.FAN_ENABLE = !SW_hbState.flags.FAN_ENABLE;
+
+	//int fan_toggle = sprintf(buff, "Fan button Pressed\r\n");
+	//HAL_UART_Transmit(&huart3, buff, fan_toggle, 2000);
+}
+
+void button_four_function() {
+	//int car_horn = sprintf(buff, "Beep Beep! get out of the way\r\n");
+	//HAL_UART_Transmit(&huart3, buff, car_horn, 2000);
+
+	SW_ActivateHorn_t msg = Compose_SW_ActivateHorn();
+	CAN_TxHeaderTypeDef header = { .ExtId = msg.id, .IDE =
+	CAN_ID_EXT, .RTR = CAN_RTR_DATA, .DLC = 0, .TransmitGlobalTime = DISABLE };
+
+	HAL_CAN_AddTxMessage(&hcan1, &header, NULL, &txMailbox_CAN1);
+}
 
 /* USER CODE END 4 */
 
